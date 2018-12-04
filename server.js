eb9cd3da139c5f5abf32819ced51c07a1e2a1ff1
Jihui yang <jihyang@dhcp-136-81.nay.redhat.com>
2018-12-04 14:09:47 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var qs = require('querystring');
+require('http').createServer(function(req,res){
+    if(req.url == '/'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end([
+            '<form method="POST" action="/url">',
+            '<h1>My form</h1>',
+            '<fieldset>',
+            '<label>Personal information</label>',
+            '<p>What is your name?</p>',
+            '<input type="text" name="name">',
+            '<p><button>Submit</button></p>',
+            '</form>'
+        ].join(''));
+    }
+    else if(req.url == '/url' && 'POST' == req.method){
+        var body = '';
+        //console.log(req);
+        req.on('data',function(chunk){
+            body += chunk;
+         });
+        req.on('end',function(){
+            res.writeHead(200,{'Content-Type':'text/html'});
+            console.log(qs.parse(body));
+	    res.end('<p>Your name is <b>' + qs.parse(body).name + '</b></p>');
+        });
+
+    }
+    else{
+        res.writeHead(404);
+        res.end('Not Found');
+    }
+}).listen(3000);
